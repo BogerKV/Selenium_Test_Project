018b1b7110fbd232302a9e0164d4fa4480a0b5d9
Ksenia <boger.k.v@mail.ru>
2019-07-31 22:16:31 +1000

    Add test test_guest_cant_see_product_in_cart_opened_from_product_page()

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.cart_page import CartPage
 import time
 import pytest
 
@@ -20,6 +21,16 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     
     login_page = LoginPage(browser, browser.current_url) # инициализируем Page Object со страницей логина
     login_page.should_be_login_page() # проверка, что страница является страницей логина
+
+@pytest.mark.cart
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()
+    page.go_to_cart()
+    cart_page = CartPage(browser, browser.current_url)
+    cart_page.should_not_be_product_in_cart()
+    cart_page.should_be_cart_is_empty_message()
     
 def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
